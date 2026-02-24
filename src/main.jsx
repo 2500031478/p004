093993ec0a9d805038b5a57c971e5dac67b023fd
2500031478 <2500031478@kluniversity.in>
2026-02-24 12:20:09 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App1.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './index.css'
 import App1 from './App1.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App/>} />
-            <Route path='/fc' element={<App1/>} />
-        </Routes>
+  <StrictMode>
+    <BrowserRouter basename="/p004">
+      <Routes>
+        <Route path="/" element={<App1 />} />
+        <Route path="/fc" element={<App1 />} />
+      </Routes>
     </BrowserRouter>
+  </StrictMode>
 )
